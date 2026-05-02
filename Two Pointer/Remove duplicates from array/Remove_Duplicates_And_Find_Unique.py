ffa66770c91f8be246d899331054c3bc47f3c4a3
Devendra kushwah <105185014+devendrakushwah80@users.noreply.github.com>
2026-05-02 20:32:34 +0530

    Add files via upload

---
+++ Two Pointer/Remove duplicates from array/Remove_Duplicates_And_Find_Unique.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        k = 1
+        j = 1
+        while j < len(nums):
+            if nums[j] != nums[i]:
+                nums[i+1] = nums[j]
+                i +=1
+            j +=1
+        
+        return i + 1
