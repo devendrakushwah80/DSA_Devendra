718737003a0e69d0f841596d0d7aa0e425bf95cd
Devendra kushwah <105185014+devendrakushwah80@users.noreply.github.com>
2026-05-08 00:08:59 +0530

    Add files via upload

---
+++ Two Pointer/Rearrange/Moves_Zero.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i =0
+        j=1
+        n = len(nums)
+        for j in range(n):
+            if nums[j]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
