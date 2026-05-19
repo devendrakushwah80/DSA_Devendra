bbb84fcde1d7f96a12cad2806a6b28df576f4828
Devendra kushwah <105185014+devendrakushwah80@users.noreply.github.com>
2026-05-19 20:59:26 +0530

    Add files via upload

---
+++ Kadanes_patten/MaxSubarraySum_kadanes.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        best = nums[0]
+        ans = nums[0]
+        for i in range(1,len(nums)):
+            v1 = best + nums[i]
+            v2 = nums[i]
+            best = max(v1,v2)
+            ans = max(ans,best)
+        return ans
