77af6dafa1db1a8087601c113f21ce03ecba358f
Devendra kushwah <105185014+devendrakushwah80@users.noreply.github.com>
2026-05-27 21:54:48 +0530

    Add files via upload

--- Normal_Questions/1678. Goal Parser Interpretation.py
+++ Normal_Questions/1678. Goal Parser Interpretation.py
@@ -1,4 +1,21 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        s = command.split()
-        print(s)
+        
+        res = []
+        i = 0
+
+        while i < len(command):
+
+            if command[i] == "G":
+                res.append("G")
+                i += 1
+
+            elif command[i:i+2] == "()":
+                res.append("o")
+                i += 2
+
+            else:
+                res.append("al")
+                i += 4
+
+        return "".join(res)
