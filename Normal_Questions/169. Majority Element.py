cbee25cb5fbde199d5d09f47d970215b38ce67a7
Devendra kushwah <105185014+devendrakushwah80@users.noreply.github.com>
2026-06-04 18:50:46 +0530

    Add files via upload

---
+++ Normal_Questions/169. Majority Element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        freq = {}
+        for i in nums:
+            if i in freq:
+                freq[i]+=1
+            else:
+                freq[i] =1
+        for i in freq:
+            if freq[i]>(len(nums)/2):
+                return i
