c051ac524ae0b10639cb520b6cd6358c5a73d553
Devendra kushwah <105185014+devendrakushwah80@users.noreply.github.com>
2026-07-12 01:46:31 +0530

    Update Remove_Elements.py

--- Two Pointer/Remove duplicates from array/Remove_Elements.py
+++ Two Pointer/Remove duplicates from array/Remove_Elements.py
@@ -1,8 +1,14 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k =0
-        for i in range(len(nums)):
-            if nums[i]!=val:
-                nums[k]=nums[i]
-                k+=1
-        return k
+        l = 0
+        for h in range(len(nums)):
+            if nums[h]!=val:
+                nums[l],nums[h]=nums[h],nums[l]
+                l+=1
+        l = 0 
+        while l<len(nums):
+            if nums[l]==val:
+                nums.pop(l)
+            else:
+                l+=1
+        return len(nums) 
